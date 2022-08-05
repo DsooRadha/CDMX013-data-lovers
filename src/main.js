6e994305d4fc6310b40fb6af505672ec8685f8c1
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-08-05 09:38:14 -0500

    jueves

--- src/main.js
+++ src/main.js
@@ -4,3 +4,35 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+/*
+loadDoc();
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();  //Crear el objeto XMLHttpRequest:
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "data/ghibli/ghibli.json");  //xhr.open(method, URL, [async, user, password])
+  xhttp.send();
+}
+*/
+dataghibli();
+function dataghibli(){
+fetch("data/ghibli/ghibli.json")
+.then (respuesta=> respuesta.json())
+.then(data=> {
+    let name= (data.films['0'].title)
+    let imagen=(data.films['0'].poster)
+    //console.log(name);
+    document.getElementById('titulo').innerHTML = name
+    document.getElementById('exp0').innerHTML = imagen
+    
+    })
+
+    
+}
+//}    for (var i = 0; i < data.length; i++)
+/*
+*/
