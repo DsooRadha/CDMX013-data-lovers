9e799da93f47cc8ab02850213f05476c019f0d3f
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-08-05 09:34:07 -0500

    jueves

--- src/data.js
+++ src/data.js
@@ -1,5 +1,14 @@
 // estas funciones son de ejemplo
 
+/*La piedra angular de AJAX es el objeto XMLHttpRequest.
+
+Crear un objeto XMLHttpRequest
+Definir una función de devolución de llamada
+Abra el objeto XMLHttpRequest
+Enviar una solicitud a un servidor
+*/
+
+
 export const example = () => {
   return 'example';
 };
