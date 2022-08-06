ff96cdc8fa94dc93381b13e202107fa48391f91b
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-08-05 19:48:05 -0500

    contiene fors que acceden a la data

--- src/main.js
+++ src/main.js
@@ -18,34 +18,55 @@ function loadDoc() {
   xhttp.send();
 }
 */
-dataghibli();
-function dataghibli() { 
-  fetch("data/ghibli/ghibli.json")
-    .then(respuesta => respuesta.json())
-    .then(data => {
+dataghibli(); //llamando a la funcion
+function dataghibli() { //declarando funcion
+  fetch("data/ghibli/ghibli.json")// a fetch le undicas la url o la ubicacion del archivo
+    .then(respuesta => respuesta.json()) //promesa 1 le indicas en que lenguaje esta el archivo
+    .then(data => {  
+// empiezas a manipular.... aca puedes hacer un console.log para que veas lo que te arroja de data y te sea mas claro porque vienen los siguientes pasos
       let name0 = (data.films['0'].title);
       let name1 = (data.films['1'].title);
-     
-    // for (let i=0; i<data.films.poster.length; i++)
-
-    // const image= document.createElement("img");
-    //image.src= "data[i].films[i].poster"
-
-
+      let director = (data.films[7]);
 
       document.getElementById('movie0').innerHTML = name0
       document.getElementById('movie1').innerHTML = name1
-     
-     console.log (name1);
-     console.log(name0);
-     //document.getElementById("movie1").innerHTML = '<img src="${data.films['1'].poster">';
-   
+      console.log(name1);
+      console.log(name0);
+      console.log(director)
 
-     name.forEach(p => {
-      console.log (p)
+
+      for (let i = 0; i < data.films.length; i++) { //ciclo for itera desde el elemento 0; hasta el largo del arreglo (-length),; y va de uno en uno (i++)
+        let titleMovie = (data.films[i].title)    //creas una variable para poder manipular despues lo que te arroja dandole la ruta de acceso a la data
+        console.log(titleMovie);
+      }
+
+      for (let i = 0; i < data.films.length; i++) {
+        let posterMovie = (data.films[i].poster)
+        console.log(posterMovie);
+      }
+
+      for (let i = 0; i < data.films.length; i++) {
+        const img = document.createElement('img');
+        img.src = data.films[i].poster["original"]//.url
+        img.className="mb-3"
+        document.getElementById("images").appendChild(img)
+      }
+
     });
-    })
-   
+
+  // const image= document.createElement("img");
+  //image.src= "data[i].films[i].poster"
+
+
+  //document.getElementById("movie1").innerHTML = '<img src="${data.films['1'].poster">';
+
+
+  //name.forEach(p => {
+  // console.log (p)
+  // });
+
+
+
 }
 
 
@@ -60,6 +81,16 @@ async function  dataAccess(){
   data.forEach(p => {
     console.log(p");
   });
+
+      for (let i=0; i<data.films.length; i++)
+     let dataAccess= (data.films[i])
+    console.log(dataAccess);
+
+
+     let element = document.getElementById('element')
+     element.innerHTML=`<p>${data.films}</p><p>${data.films.people.director}</p>`
+     ;
+     console.log (dataghibli)
 }*/
 //}    for (var i = 0; i < data.length; i++)
 /*
