1f0b42db3b3986cf61b2494e07d49b8df45d3cd5
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-08-18 16:33:05 -0500

    funcioandofiltromanual y render de allperson, y directores, etc en main y data css y html

--- src/data.js
+++ src/data.js
@@ -1,51 +1,54 @@
 import ghibli from './data/ghibli/ghibli.js';
-
- export const dataGhibliJs = (ghibli.films);
+const allTitlesMovies = [];
+console.log(allTitlesMovies);
+export const dataGhibliJs = (ghibli.films);
 //console.log(dataGhibliJs)
 dataGhibliJs.forEach(data => {
-  const character= data.people
-  character.forEach(p=>{
- console.log(data.title, p.name , p.img)
+  const character = data.people
+  character.forEach(p => {
+    console.log(data.title, p.name, p.img)
+    const titleMovies = data.title
+    allTitlesMovies.push(titleMovies)
   });
 });
 
 export const directors = [
-  {name: "Isao Takahata", selector: '.columnIsao', imgSrc: "./images/isao.png"},
-  {name: "Hayao Miyazaki", selector: '.columnHayao', imgSrc: "./images/hayo.png"},
-  {name: "Yoshifumi Kondō", selector: '.columnYoshifumi', imgSrc: "./images/yoshifumi.png"},
-  {name: "Hiroyuki Morita", selector: '.columnHiroyuki', imgSrc: "./images/hiroyuki.png"},
-  {name: "Gorō Miyazaki", selector: '.columnGoro', imgSrc: "./images/Goro.png"},
-  {name: "Hiromasa Yonebayashi", selector: '.columnHiromasa', imgSrc: "./images/Hiromasa.png"},
+  { name: "Isao Takahata", selector: '.columnIsao', imgSrc: "./images/isao.png" },
+  { name: "Hayao Miyazaki", selector: '.columnHayao', imgSrc: "./images/hayo.png" },
+  { name: "Yoshifumi Kondō", selector: '.columnYoshifumi', imgSrc: "./images/yoshifumi.png" },
+  { name: "Hiroyuki Morita", selector: '.columnHiroyuki', imgSrc: "./images/hiroyuki.png" },
+  { name: "Gorō Miyazaki", selector: '.columnGoro', imgSrc: "./images/Goro.png" },
+  { name: "Hiromasa Yonebayashi", selector: '.columnHiromasa', imgSrc: "./images/Hiromasa.png" },
 ]
 
 export const title = [
-  {name: "Castle in the Sky", selector: '.columnCastle'},
-  {name: "My Neighbor Totoro", selector: '.columnTotoro'},
-  {name: "Kiki's Delivery Service", selector: '.columnKikis'},
-  {name: "Grave of the Fireflies", selector: '.columnGrave'},
-  {name: "Only Yesterday", selector: '.columnOnly'},
-  {name: "Porco Rosso", selector: '.columnPorco'},
-  {name: "Pom Poko", selector: '.columnPom'},
-  {name: "Whisper of the Heart", selector: '.columnWhispe'},
-  {name: "Princess Mononoke", selector: '.columnPrincess'},
-  {name: "My Neighbors the Yamadas", selector: '.columnYamadas'},
-  {name: "Spirited Away", selector: '.columnSpirited'},
-  {name: "The Cat Returns", selector: '.columnTheCat'},
-  {name: "Howl's Moving Castle", selector: '.columnHowl"'},
-  {name: "Tales from Earthsea", selector: '.columnTales'},
-  {name: "Ponyo on the Cliff by the Sea", selector: '.columnPonyo'},
-  {name: "The Secret World of Arrietty", selector: '.columnArrietty'},
-  {name: "From Up on Poppy Hill", selector: '.columnPoppyhill'},
-  {name: "The Wind Rises", selector: '.columnThewind'},
-  {name: "The Tale of the Princess Kaguya", selector: '.columnTale'},
-  {name: "When Marnie Was There", selector: '.columnMarnie"'},
+  { name: "Castle in the Sky", selector: '.columnCastle' },
+  { name: "My Neighbor Totoro", selector: '.columnTotoro' },
+  { name: "Kiki's Delivery Service", selector: '.columnKikis' },
+  { name: "Grave of the Fireflies", selector: '.columnGrave' },
+  { name: "Only Yesterday", selector: '.columnOnly' },
+  { name: "Porco Rosso", selector: '.columnPorco' },
+  { name: "Pom Poko", selector: '.columnPom' },
+  { name: "Whisper of the Heart", selector: '.columnWhispe' },
+  { name: "Princess Mononoke", selector: '.columnPrincess' },
+  { name: "My Neighbors the Yamadas", selector: '.columnYamadas' },
+  { name: "Spirited Away", selector: '.columnSpirited' },
+  { name: "The Cat Returns", selector: '.columnTheCat' },
+  { name: "Howl's Moving Castle", selector: '.columnHowl"' },
+  { name: "Tales from Earthsea", selector: '.columnTales' },
+  { name: "Ponyo on the Cliff by the Sea", selector: '.columnPonyo' },
+  { name: "The Secret World of Arrietty", selector: '.columnArrietty' },
+  { name: "From Up on Poppy Hill", selector: '.columnPoppyhill' },
+  { name: "The Wind Rises", selector: '.columnThewind' },
+  { name: "The Tale of the Princess Kaguya", selector: '.columnTale' },
+  { name: "When Marnie Was There", selector: '.columnMarnie"' },
 ]
- 
+
 
 //PASOS A SEGUIR CUANDO QUIERO ELIMINAR DATA REPETIDA
-const allDataGhibli= dataGhibliJs.map(({ director}) => { return ({ director }).director })//1.hago un Array con los elementos a analizar de dataJS
+const allDataGhibli = dataGhibliJs.map(({ director }) => { return ({ director }).director })//1.hago un Array con los elementos a analizar de dataJS
 const director = [];            // 2. creo un array vacio donde llegaran los datos ya sin repetir 
-allDataGhibli.forEach( (elemento) => {   // 3. sustituyo el elemento a iterar "allDirector" 
+allDataGhibli.forEach((elemento) => {   // 3. sustituyo el elemento a iterar "allDirector" 
   if (!director.includes(elemento)) {
     director.push(elemento);
   }
@@ -58,12 +61,15 @@ export const filterDirector = function (name) { //function (data,directorName)
   return directors
 }
 
-const character= dataGhibliJs.filter(p => p.title == "Castle in the Sky")
-  character.forEach(p=>{
-   const people= p.people
-   people.forEach(character=>
-    { console.log(character.name,character.img)  })
+export const arrayExperimento = []
+const character = dataGhibliJs.filter(p => p.title == "My Neighbor Totoro")
+character.forEach(p => {
+  const people = p.people
+  people.forEach(character => {
+    console.log(character)
+    arrayExperimento.push(character)
   })
+})
 
 /*
   export const filterFilmsPeople= function (name){
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import { filterDirector, directors} from "./data.js";
+import { filterDirector, directors, dataGhibliJs, arrayExperimento } from "./data.js";
 //import { title,  filterPersonajes} from "./data.js";
-import { dataGhibliJs} from "./data.js";
-//import{filterFilmsPeople} from "./data.js";
- 
+console.log(arrayExperimento)
 
 dataAccess();
 function dataAccess() {
@@ -23,7 +21,7 @@ function dataAccess() {
   });
   return (dataghibli)
 }
- 
+
 const renderDirector = (directorName, columnDirectorSelector) => {
   const director = filterDirector(directorName)
   director.forEach(d => {
@@ -34,22 +32,30 @@ const renderDirector = (directorName, columnDirectorSelector) => {
     columnDirectors.appendChild(div);
   })
 }
+
 directors.forEach(director => renderDirector(director.name, director.selector))
 
-//filterFilmsPeople()
 
-dataGhibliJs.forEach (data => {
-    const character= data.people
-    character.forEach(p=>{
-    console.log (p.name, p.img)
-    let columnCharacter = document.querySelector(".allMovies");
+dataGhibliJs.forEach(data => {
+  const character = data.people
+  character.forEach(p => {
+    console.log(p.name, p.img)
+
+    let columnCharacter = document.querySelector(".movies");
     let div = document.createElement('div');    //creamos un elemento div por cada elemento del array
     div.setAttribute('id', 'title');    //le asignamos el id 'card' a cada div
-    div.innerHTML = `<img class="poster"src="${p.img}"><h2> ${p.name}</h2><h6>${p.gender}</h6>`;
+    div.innerHTML = `<img class="poster"src="${p.img}"><h2>${p.name}</h2><h2> ${p.gender}</h2>`;
     columnCharacter.appendChild(div);
   })
 })
- 
+
+arrayExperimento.forEach(item =>{
+const loqueaparececuandovanalbtnTotoro = document.querySelector (".columnTotoro");
+let div = document.createElement('div');
+div.setAttribute('id', 'title'); 
+div.innerHTML = `<img class="poster"src="${item.img}"><h2>${item.name}</h2><h2> ${item.gender}</h2>`;
+loqueaparececuandovanalbtnTotoro.appendChild(div);
+})
 /*
 const renderPersonajes = (filmsName, columntitleSelector) => {
   const title = filterPersonajes(filmsName)
@@ -66,98 +72,104 @@ title.forEach(films => renderPersonajes(films.name, films.selector))
 
 const btnHayao = document.getElementById("Hayao");
 btnHayao.addEventListener("click", function (e) {
-  e.preventDefault() 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "none";
-  document.querySelector(".columnHayao").style.display= "grid";
-  document.querySelector(".columnYoshifumi").style.display= "none";
-  document.querySelector(".columnHiroyuki").style.display= "none";
-  document.querySelector(".columnGoro").style.display= "none";
-  document.querySelector(".columnHiromasa").style.display= "none";
+  document.querySelector(".columnIsao").style.display = "none";
+  document.querySelector(".columnHayao").style.display = "grid";
+  document.querySelector(".columnYoshifumi").style.display = "none";
+  document.querySelector(".columnHiroyuki").style.display = "none";
+  document.querySelector(".columnGoro").style.display = "none";
+  document.querySelector(".columnHiromasa").style.display = "none";
 });
 
 const btnIsao = document.getElementById("Isao");
-btnIsao.addEventListener("click", function (e) {e.preventDefault() 
+btnIsao.addEventListener("click", function (e) {
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "grid";
-  document.querySelector(".columnHayao").style.display= "none";
-  document.querySelector(".columnHiroyuki").style.display= "none";
-  document.querySelector(".columnGoro").style.display= "none";
-  document.querySelector(".columnHiromasa").style.display= "none";
+  document.querySelector(".columnIsao").style.display = "grid";
+  document.querySelector(".columnHayao").style.display = "none";
+  document.querySelector(".columnHiroyuki").style.display = "none";
+  document.querySelector(".columnGoro").style.display = "none";
+  document.querySelector(".columnHiromasa").style.display = "none";
 });
 
 const btnYoshifumi = document.getElementById("Yoshifumi");
 btnYoshifumi.addEventListener("click", function (e) {
-  e.preventDefault() 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "none";
-  document.querySelector(".columnHayao").style.display= "none";
-  document.querySelector(".columnYoshifumi").style.display= "grid";
-  document.querySelector(".columnHiroyuki").style.display= "none";
-  document.querySelector(".columnGoro").style.display= "none";
-  document.querySelector(".columnHiromasa").style.display= "none";
+  document.querySelector(".columnIsao").style.display = "none";
+  document.querySelector(".columnHayao").style.display = "none";
+  document.querySelector(".columnYoshifumi").style.display = "grid";
+  document.querySelector(".columnHiroyuki").style.display = "none";
+  document.querySelector(".columnGoro").style.display = "none";
+  document.querySelector(".columnHiromasa").style.display = "none";
 });
 
 
 const btnHiroyuki = document.getElementById("Hiroyuki");
 btnHiroyuki.addEventListener("click", function (e) {
-  e.preventDefault() 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "none";
-  document.querySelector(".columnHayao").style.display= "none";
-  document.querySelector(".columnYoshifumi").style.display= "none";
-  document.querySelector(".columnHiroyuki").style.display= "grid";
-  document.querySelector(".columnGoro").style.display= "none";
-  document.querySelector(".columnHiromasa").style.display= "none";
+  document.querySelector(".columnIsao").style.display = "none";
+  document.querySelector(".columnHayao").style.display = "none";
+  document.querySelector(".columnYoshifumi").style.display = "none";
+  document.querySelector(".columnHiroyuki").style.display = "grid";
+  document.querySelector(".columnGoro").style.display = "none";
+  document.querySelector(".columnHiromasa").style.display = "none";
 });
 
 
 const btnGoro = document.getElementById("Goro");
 btnGoro.addEventListener("click", function (e) {
-  e.preventDefault() 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "none";
-  document.querySelector(".columnHayao").style.display= "none";
-  document.querySelector(".columnYoshifumi").style.display= "none";
-  document.querySelector(".columnHiroyuki").style.display= "none";
-  document.querySelector(".columnGoro").style.display= "grid";
-  document.querySelector(".columnHiromasa").style.display= "none";
+  document.querySelector(".columnIsao").style.display = "none";
+  document.querySelector(".columnHayao").style.display = "none";
+  document.querySelector(".columnYoshifumi").style.display = "none";
+  document.querySelector(".columnHiroyuki").style.display = "none";
+  document.querySelector(".columnGoro").style.display = "grid";
+  document.querySelector(".columnHiromasa").style.display = "none";
 });
 
 const btnHiromasa = document.getElementById("Hiromasa");
 btnHiromasa.addEventListener("click", function (e) {
-  e.preventDefault() 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
   document.querySelector(".all").style.display = "grid";
-  document.querySelector(".columnIsao").style.display= "none";
-  document.querySelector(".columnHayao").style.display= "none";
-  document.querySelector(".columnYoshifumi").style.display= "none";
-  document.querySelector(".columnHiroyuki").style.display= "none";
-  document.querySelector(".columnGoro").style.display= "none";
-  document.querySelector(".columnHiromasa").style.display= "grid";
+  document.querySelector(".columnIsao").style.display = "none";
+  document.querySelector(".columnHayao").style.display = "none";
+  document.querySelector(".columnYoshifumi").style.display = "none";
+  document.querySelector(".columnHiroyuki").style.display = "none";
+  document.querySelector(".columnGoro").style.display = "none";
+  document.querySelector(".columnHiromasa").style.display = "grid";
 });
-
 
 const btnFilms0 = document.getElementById("castle");
 btnFilms0.addEventListener("click", function (e) {
-  e.preventDefault() 
- 
+  e.preventDefault()
   document.querySelector(".gridAll").style.display = "none";
-  document.querySelector(".all").style.display = "grid";
- 
+  document.querySelector(".all").style.display = "none";
+  document.querySelector(".movies").style.display = "grid";
 });
 
+
+const btnFilms1 = document.getElementById("totoro");
+btnFilms1.addEventListener("click", function (e) {
+  e.preventDefault()
+  document.querySelector(".gridAll").style.display = "none";
+  document.querySelector(".all").style.display = "none";
+  document.querySelector(".columTotoro").style.display = "grid";
+});
 
 const renderDirectorImage = (directorName) => {
   const director = directors.filter(director => director.name === directorName)[0]
   return `
-    <img id="imgHayao" src="${director.imgSrc}" alt="${director.name}" style="display: block;">
-  `;
+    <img id="imgHayao" src="${director.imgSrc}" alt="${director.name}" style="display: block;">`;
 }
 
 const directorSpaceRightElement = document.querySelector('.spaceRigth')
